e4600f0e7d57716e6a06af77484a18c27d4ce86e
Pranjal birla <birlapranjal460@gmail.com>
2024-09-09 09:39:55 +0530

    first commit

---
+++ public/data/faqs.ts
@@ -0,0 +1,22 @@
+export const faqs: Array<{ id: number; ques: string; desc: string }> = [
+  {
+    id: 1,
+    ques: "What are the activities?",
+    desc: "Activities conducted by Abhyudaya include real-life projects, industry engagements, seminars/workshops, and hackathons. Collectively, all activities are aimed at enhancing skills, learning, and promoting innovation.",
+  },
+  {
+    id: 2,
+    ques: "What is Abhyudaya?",
+    desc: "The SVIIT Coding Club offers students a dynamic platform to learn, practice, and excel in coding. it welcomes individuals of all skill levels. Through engaging in coding competitions, hackathons, and a shared passion for technology, the club transforms ideas into reality. Every member, whether novice or experienced, contributes and is valued, creating a welcoming space for growth, knowledge-sharing, and inspiration in the exciting realm of coding and technology.",
+  },
+  {
+    id: 3,
+    ques: "What is the recruitment process??",
+    desc: "The SVIIT Coding Club offers students a dynamic platform to learn, practice, and excel in coding. it welcomes individuals of all skill levels. Through engaging in coding competitions, hackathons, and a shared passion for technology, the club transforms ideas into reality. Every member, whether novice or experienced, contributes and is valued, creating a welcoming space for growth, knowledge-sharing, and inspiration in the exciting realm of coding and technology.",
+  },
+  {
+    id: 4,
+    ques: "How to participate in club?",
+    desc: "The SVIIT Coding Club offers students a dynamic platform to learn, practice, and excel in coding. it welcomes individuals of all skill levels. Through engaging in coding competitions, hackathons, and a shared passion for technology, the club transforms ideas into reality. Every member, whether novice or experienced, cont",
+  },
+];
---
+++ public/data/upcomingEvent.ts
@@ -0,0 +1,15 @@
+import img from "@/../public/arvr.jpg";
+
+export const events: Array<{
+  id: number;
+  heading: string;
+  desc: string;
+  img: any;
+}> = [
+  {
+    id: 1,
+    heading: "AR VR Workshop",
+    desc: "The AR/VR workshop on September 12th, organized in collaboration with VRadicals and the Abhyudaya Coding Club, will focus on guiding participants on how to get started with AR/VR development. The workshop will cover the benefits of working in this innovative field, provide insights into the tools and technologies required, and explore various applications and opportunities in the AR/VR domain. Participants will also gain a deeper understanding of the future potential of immersive technologies.",
+    img: img,
+  },
+];
